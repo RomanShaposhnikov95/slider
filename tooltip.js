48de718da2f61bd030890d19777f729a95ae959d
Roman Shaposhnikov <romanshaposhnikov95@gmail.com>
2023-08-03 11:12:09 +0300

    add tooltip

---
+++ tooltip.js
@@ -0,0 +1,28 @@
+function adjustTooltipPosition(tooltip) {
+    const coord = tooltip.getBoundingClientRect();
+
+    if (coord.x < 20) {
+        tooltip.style.left = "0";
+        tooltip.style.right = "unset";
+        tooltip.style.transform = "unset";
+    }
+
+    if (coord.right > window.innerWidth - 20) {
+        tooltip.style.left = "unset";
+        tooltip.style.right = "0";
+        tooltip.style.transform = "unset";
+    }
+}
+
+function handleWindowResize() {
+    const tooltips = document.querySelectorAll(".tooltip");
+
+    tooltips.forEach(tooltip => {
+        adjustTooltipPosition(tooltip);
+    });
+}
+
+
+window.addEventListener("load", handleWindowResize);
+
+window.addEventListener("resize", handleWindowResize);
